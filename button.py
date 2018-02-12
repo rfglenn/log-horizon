7b0e30f1b64640a5ac7b39eea50d13638bc413b1
rglenn <roryfglenn@gmail.com>
2018-02-11 16:04:38 -0800

    log to a file

--- button.py
+++ button.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from datetime import datetime
 from gpiozero import LED
 from gpiozero import Button
@@ -6,15 +7,19 @@ from gpiozero import Button
 button = Button(26)
 led = LED(12)
 
+def main():
+    with open(sys.argv[1], "a") as f:
+        while True:
+            button.wait_for_press()
+            led.on()
+            f.write('%s\t' % (datetime.now(),))
+            time.sleep(0.3)
 
+            button.wait_for_press()
+            led.off()
+            f.write('%s\n' % (datetime.now(),))
+            time.sleep(0.3)
+            f.flush()
 
-while True:
-	button.wait_for_press()
-	led.on()
-	print('on: ', datetime.now())
-	time.sleep(0.3)
-
-	button.wait_for_press()
-	led.off()
-	print('off: ', datetime.now())
-	time.sleep(0.3)
+if __name__ == '__main__':
+    main()
